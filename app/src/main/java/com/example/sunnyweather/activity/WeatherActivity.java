95b7ac91586929bcf76156daf8fa0af48f5237de
Kun <413164995@qq.com>
2016-09-12 16:26:36 +0800

    更新未来三天天气状况

--- app/src/main/java/com/example/sunnyweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/sunnyweather/activity/WeatherActivity.java
@@ -1,14 +1,19 @@
 package com.example.sunnyweather.activity;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,6 +22,8 @@ import com.example.sunnyweather.service.AutoUpdateService;
 import com.example.sunnyweather.util.HttpCallbackListener;
 import com.example.sunnyweather.util.HttpUtil;
 import com.example.sunnyweather.util.Utility;
+
+import java.util.Calendar;
 
 /**
  * Created by admin on 2016/9/7.
@@ -54,11 +61,44 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
      */
     private Button refreshWeather;
 
+    /**
+     *显示湿度风力
+     */
+    private TextView nowCond;
+
+    /**
+     *天气图片
+     */
+    private ImageView weather_pic1;
+    private ImageView weather_pic2;
+    private ImageView weather_pic3;
+
+    /**
+     *三天天气描述
+     */
+    private TextView max1;
+    private TextView min1;
+    private TextView daily_forecast_desp1;
+    private TextView max2;
+    private TextView min2;
+    private TextView daily_forecast_desp2;
+    private TextView max3;
+    private TextView min3;
+    private TextView daily_forecast_desp3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.weather_layout);
+        View v = findViewById(R.id.title_top);
+        v.getBackground().setAlpha(120);
+        View v1 = findViewById(R.id.first_day_l);
+        v1.getBackground().setAlpha(80);
+        View v2 = findViewById(R.id.second_day_l);
+        v2.getBackground().setAlpha(100);
+        View v3 = findViewById(R.id.third_day_l);
+        v3.getBackground().setAlpha(80);
         //初始化各种控件
         weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
         cityNameText = (TextView) findViewById(R.id.city_name);
@@ -68,6 +108,19 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         currentDateText = (TextView) findViewById(R.id.current_date);
         switchCity = (Button) findViewById(R.id.switch_city);
         refreshWeather = (Button) findViewById(R.id.refresh_weather);
+        nowCond = (TextView) findViewById(R.id.now_cond);
+        weather_pic1 = (ImageView)findViewById(R.id.weather_pic1);
+        weather_pic2 = (ImageView)findViewById(R.id.weather_pic2);
+        weather_pic3 = (ImageView)findViewById(R.id.weather_pic3);
+        max1 = (TextView) findViewById(R.id.max1);
+        min1 = (TextView) findViewById(R.id.min1);
+        daily_forecast_desp1 = (TextView) findViewById(R.id.daily_forecast_desp1);
+        max2 = (TextView) findViewById(R.id.max2);
+        min2 = (TextView) findViewById(R.id.min2);
+        daily_forecast_desp2 = (TextView) findViewById(R.id.daily_forecast_desp2);
+        max3 = (TextView) findViewById(R.id.max3);
+        min3 = (TextView) findViewById(R.id.min3);
+        daily_forecast_desp3 = (TextView) findViewById(R.id.daily_forecast_desp3);
         switchCity.setOnClickListener(this);
         refreshWeather.setOnClickListener(this);
         String cityId = getIntent().getStringExtra("city_code");
@@ -116,7 +169,134 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         tempText.setText(preferences.getString("temp","")+"℃");
         weatherDespText.setText(preferences.getString("weather_desp",""));
         publishText.setText("今天"+preferences.getString("publish_time","")+"发布");
-        currentDateText.setText(preferences.getString("current_data", ""));
+        currentDateText.setText(preferences.getString("current_date", ""));
+        nowCond.setText(preferences.getString("nowCond",""));
+        max1.setText(preferences.getString("daily1_temp_max","")+"℃");
+        max2.setText(preferences.getString("daily2_temp_max","")+"℃");
+        max3.setText(preferences.getString("daily3_temp_max","")+"℃");
+        min1.setText(preferences.getString("daily1_temp_min","")+"℃");
+        min2.setText(preferences.getString("daily2_temp_min","")+"℃");
+        min3.setText(preferences.getString("daily3_temp_min","")+"℃");
+        //判断系统是12小时制还是24小时制
+        ContentResolver cv = getBaseContext().getContentResolver();
+        String strTimeFormat = android.provider.Settings.System.getString(cv, android.provider.Settings.System.TIME_12_24);
+        Calendar mCalendar=Calendar.getInstance();
+        long time=System.currentTimeMillis();
+        mCalendar.setTimeInMillis(time);
+        if(strTimeFormat!=null && strTimeFormat.equals("24"))
+        {
+            int mHour=mCalendar.get(Calendar.HOUR)+12;
+            if(19<=mHour&&mHour<=24||0<=mHour&&mHour<=5){
+                String desp_n = preferences.getString("daily1_cond_n","");
+                String desp_n2 = preferences.getString("daily2_cond_n","");
+                String desp_n3 = preferences.getString("daily3_cond_n","");
+                switch (desp_n){
+                    case "晴":
+                        weather_pic1.setImageResource(R.drawable.sunny_night);
+                        break;
+                    case "多云":
+                        weather_pic1.setImageResource(R.drawable.cloudy_night);
+                        break;
+                    case "阵雨":
+                        weather_pic1.setImageResource(R.drawable.shower_night);
+                        break;
+                    case "雷阵雨":
+                        weather_pic1.setImageResource(R.drawable.tstorm_night);
+                }
+                switch (desp_n2){
+                    case "晴":
+                        weather_pic2.setImageResource(R.drawable.sunny_night);
+                        break;
+                    case "多云":
+                        weather_pic2.setImageResource(R.drawable.cloudy_night);
+                        break;
+                    case "阵雨":
+                        weather_pic2.setImageResource(R.drawable.shower_night);
+                        break;
+                    case "雷阵雨":
+                        weather_pic2.setImageResource(R.drawable.tstorm_night);
+                }
+                switch (desp_n3){
+                    case "晴":
+                        weather_pic3.setImageResource(R.drawable.sunny_night);
+                        break;
+                    case "多云":
+                        weather_pic3.setImageResource(R.drawable.cloudy_night);
+                        break;
+                    case "阵雨":
+                        weather_pic3.setImageResource(R.drawable.shower_night);
+                        break;
+                    case "雷阵雨":
+                        weather_pic3.setImageResource(R.drawable.tstorm_night);
+                }
+                daily_forecast_desp1.setText(desp_n);
+                daily_forecast_desp2.setText(desp_n2);
+                daily_forecast_desp3.setText(desp_n3);
+            }else {
+                String desp_d = preferences.getString("daily1_cond_d","");
+                String desp_d2 = preferences.getString("daily2_cond_d","");
+                String desp_d3 = preferences.getString("daily3_cond_d","");
+                switch (desp_d){
+                    case "晴":
+                        weather_pic1.setImageResource(R.drawable.sunny);
+                        break;
+                    case "多云":
+                        weather_pic1.setImageResource(R.drawable.cloudy);
+                        break;
+                    case "阵雨":
+                        weather_pic1.setImageResource(R.drawable.shower);
+                        break;
+                    case "雷阵雨":
+                        weather_pic1.setImageResource(R.drawable.tstorm);
+                        break;
+                }
+                switch (desp_d2){
+                    case "晴":
+                        weather_pic2.setImageResource(R.drawable.sunny);
+                        break;
+                    case "多云":
+                        weather_pic2.setImageResource(R.drawable.cloudy);
+                        break;
+                    case "阵雨":
+                        weather_pic2.setImageResource(R.drawable.shower);
+                        break;
+                    case "雷阵雨":
+                        weather_pic2.setImageResource(R.drawable.tstorm);
+                        break;
+                }
+                switch (desp_d3){
+                    case "晴":
+                        weather_pic3.setImageResource(R.drawable.sunny);
+                        break;
+                    case "多云":
+                        weather_pic3.setImageResource(R.drawable.cloudy);
+                        break;
+                    case "阵雨":
+                        weather_pic3.setImageResource(R.drawable.shower);
+                        break;
+                    case "雷阵雨":
+                        weather_pic3.setImageResource(R.drawable.tstorm);
+                        break;
+                }
+                daily_forecast_desp1.setText(desp_d);
+                daily_forecast_desp2.setText(desp_d2);
+                daily_forecast_desp3.setText(desp_d3);
+            }
+        }
+        //12小时制暂时不写
+        else
+        {
+            String amPmValues;
+            Calendar c = Calendar.getInstance();
+            if(c.get(Calendar.AM_PM) == 0)
+            {
+                amPmValues = "AM";
+            }
+            else
+            {
+                amPmValues = "PM";
+            }
+        }
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
         Intent intent = new Intent(this, AutoUpdateService.class);
--- app/src/main/java/com/example/sunnyweather/util/Utility.java
+++ app/src/main/java/com/example/sunnyweather/util/Utility.java
@@ -15,6 +15,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -62,27 +63,77 @@ public class Utility {
         String cityId = basicBean.getHeWeather_data_service().get(0).getBasic().getId();
         //天气状况描述
         String weatherDesp = basicBean.getHeWeather_data_service().get(0).getNow().getCond().getTxt();
-        List<Bean.HeWeatherDataServiceBean.HourlyForecastBean> list = basicBean.getHeWeather_data_service().get(0).getHourly_forecast();
-        int i = list.size()-1;
         //气温
-        String temp = list.get(i).getTmp();
+        String temp = basicBean.getHeWeather_data_service().get(0).getNow().getTmp();
         //更新时间
-        String publishTime = list.get(i).getDate();
-        saveWeatherInfo(context,cityName,cityId,temp,weatherDesp,publishTime);
+        String publishTime = basicBean.getHeWeather_data_service().get(0).getBasic().getUpdate().getLoc();
+        //湿度风力
+        String nowHum = basicBean.getHeWeather_data_service().get(0).getNow().getHum();
+        String nowWind = basicBean.getHeWeather_data_service().get(0).getNow().getWind().getDir()+basicBean.getHeWeather_data_service().get(0).getNow().getWind().getSc();
+        String nowCond = "湿度"+nowHum+"%"+" "+nowWind+"级";
+        /**
+         * 三天天气
+         */
+        //白天天气状况
+        String daily1_cond_d = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(0).getCond().getTxt_d();
+        String daily2_cond_d = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(1).getCond().getTxt_d();
+        String daily3_cond_d = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(2).getCond().getTxt_d();
+        //夜晚天气状况
+        String daily1_cond_n = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(0).getCond().getTxt_n();
+        String daily2_cond_n = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(1).getCond().getTxt_n();
+        String daily3_cond_n = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(2).getCond().getTxt_n();
+        //未来三天气温描述
+        String daily1_temp_max = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(0).getTmp().getMax();
+        String daily1_temp_min = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(0).getTmp().getMin();
+        String daily2_temp_max = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(1).getTmp().getMax();
+        String daily2_temp_min = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(1).getTmp().getMin();
+        String daily3_temp_max = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(2).getTmp().getMax();
+        String daily3_temp_min = basicBean.getHeWeather_data_service().get(0).getDaily_forecast().get(2).getTmp().getMin();
+        saveWeatherInfo(context,cityName,cityId,temp,weatherDesp,publishTime,nowCond,daily1_cond_d,daily1_cond_n,daily1_temp_max,daily1_temp_min,daily2_cond_d,daily2_cond_n,daily2_temp_max,daily2_temp_min,daily3_cond_d,daily3_cond_n,daily3_temp_max,daily3_temp_min);
     }
     /**
      * 将服务器返回的所有天气信息存储到SharedPreFerences文件中
      */
-    public static void saveWeatherInfo(Context context,String cityName,String cityId,String temp,String weatherDesp,String publishTime){
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+    public static void saveWeatherInfo(Context context,String cityName,String cityId,String temp,String weatherDesp,String publishTime,String nowCond,String daily1_cond_d,String daily1_cond_n,String daily1_temp_max,String daily1_temp_min,String daily2_cond_d,String daily2_cond_n,String daily2_temp_max,String daily2_temp_min,String daily3_cond_d,String daily3_cond_n,String daily3_temp_max,String daily3_temp_min){
+        SimpleDateFormat sdf = new SimpleDateFormat("M/d", Locale.CHINA);
+        final Calendar c = Calendar.getInstance();
+        String mWay = String.valueOf(c.get(Calendar.DAY_OF_WEEK));
+        if("1".equals(mWay)){
+            mWay ="天";
+        }else if("2".equals(mWay)){
+            mWay ="一";
+        }else if("3".equals(mWay)){
+            mWay ="二";
+        }else if("4".equals(mWay)){
+            mWay ="三";
+        }else if("5".equals(mWay)){
+            mWay ="四";
+        }else if("6".equals(mWay)){
+            mWay ="五";
+        }else if("7".equals(mWay)){
+            mWay ="六";
+        }
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
-        editor.putBoolean("city_selected",true);
-        editor.putString("city_name",cityName);
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
         editor.putString("city_id",cityId);
         editor.putString("temp",temp);
-        editor.putString("weather_desp",weatherDesp);
+        editor.putString("weather_desp", weatherDesp);
         editor.putString("publish_time",publishTime);
-        editor.putString("current_date",sdf.format(new Date()));
+        editor.putString("nowCond",nowCond);
+        editor.putString("current_date",(sdf.format(new Date())+"|周"+mWay));
+        editor.putString("daily1_cond_d",daily1_cond_d);
+        editor.putString("daily1_cond_n",daily1_cond_n);
+        editor.putString("daily1_temp_max",daily1_temp_max);
+        editor.putString("daily1_temp_min",daily1_temp_min);
+        editor.putString("daily2_cond_d",daily2_cond_d);
+        editor.putString("daily2_cond_n",daily2_cond_n);
+        editor.putString("daily2_temp_max",daily2_temp_max);
+        editor.putString("daily2_temp_min",daily2_temp_min);
+        editor.putString("daily3_cond_d",daily3_cond_d);
+        editor.putString("daily3_cond_n",daily3_cond_n);
+        editor.putString("daily3_temp_max",daily3_temp_max);
+        editor.putString("daily3_temp_min",daily3_temp_min);
         editor.commit();
     }
 }
